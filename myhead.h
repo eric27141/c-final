97d9958a798ad0b004f1b9256a367f784ab2bfef
eric27141 <eric20050127@gmail.com>
2024-06-25 15:20:56 +0800

    myhead.h

--- myhead.h
+++ myhead.h
@@ -2,26 +2,37 @@
 #define MYHEAD_H
 
 #include <stdio.h>
+#include "rlutil.h"
 
+
+// 定義學生結構
 typedef struct {
     char name[50];
     char department[50];
     int grade;
     char sid[50];
-
 } Student;
 
-void randominitStudents (Student students[], int numStudents);
+typedef struct {
+    Student student;
+    // 作業,平時測驗,期中測驗,期末報告,其他(出席及平時表現),原始成績
+    double hw, quiz, midterm, final, other, total;
+} SRecord;
 
-//void sortStudentsByname(Student* students, int numStudents);
-//void sortStudentsBydepartment(Student* students, int numStudents);
-//void sortStudentsBygrade(Student* students, int numStudents);
+#define HW_WEIGHT 0.2
+#define QUIZ_WEIGHT 0.1
+#define MIDTERM_WEIGHT 0.3
+#define FINAL_WEIGHT 0.3
+#define OTHER_WEIGHT 0.1
+
+void printStudent(Student students[], int numStudents); 
+void printSRecords(SRecord srecords[], int numSRecords);
+int readSRecordsFromCSV(const char* filename, SRecord srecords[], int maxSRecords);
+
+void randominitStudents(Student students[], int numStudents);
 //void sortStudentsBySID(Student* students, int numStudents);
+//void sortStudentsByName(Student* students, int numStudents);
 void sortStudents(Student* students, int numStudents, const char* sortBy);
-
-void printStudentsByname(Student students[], int numStudents);
-void printStudentsBydepartment(Student students[], int numStudents);
-void printStudentsBygrade(Student students[], int numStudents);
-void printStudentsBySID(Student students[], int numStudents);
+void sortSRecords(SRecord* srecords, int numSRecords, const char* sortBy);
 
 #endif // MYHEAD_H
